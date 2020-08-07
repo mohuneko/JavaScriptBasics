5786f6dce885849911478c8648de440171777a76
mohuneko <b0wdmohu@gmail.com>
2020-08-07 23:14:25 +0900

    first

---
+++ js/min.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// console.log('Hello');
+// console.log("Hello");
+
+// console.log('it's me!');
+// console.log("it\'s me!");
+
+// console.log('hel\nlo wor\tld!');
+
+// console.log('hello' + 'world');
+
+// console.log(10 + 3); //13
+// console.log(10 - 3); // 7
+// console.log(10 * 3); // 30
+// console.log(10 / 3); //3.3333
+// console.log(10 % 3); //1
+// console.log(10 ** 3); //10の3乗
+
+// console.log(2 + 10 * 3); //32
+// console.log((2 + 10) * 3); //36
+
+//定数
