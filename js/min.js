115cf140de12d561ab248d4e9e48cd25cece48df
mohuneko <b0wdmohu@gmail.com>
2020-08-08 23:12:26 +0900

    真偽値

--- js/min.js
+++ js/min.js
@@ -52,8 +52,30 @@
 
 // console.log(price);
 
-console.log(typeof 'hello');
-console.log(typeof 5);
-console.log(typeof true);
-console.log(typeof undefined);
-console.log(typeof null);
+// console.log(typeof 'hello');
+// console.log(typeof 5);
+// console.log(typeof true);
+// console.log(typeof undefined);
+// console.log(typeof null);
+
+// console.log('5' * 3);
+// console.log('5' - '3');
+// console.log(parseInt('5', 10) + 3);
+
+// console.log(parseInt('hello', 10));
+
+// const price = 1200;
+
+// console.log(price > 1000); //true
+// console.log(price < 1000); //false
+// console.log(price >= 1000); //true
+// console.log(price <= 1000); //false
+// console.log(price === 1000); //false
+// console.log(price !== 1000); //true
+
+
+//false <- 0, null, undefined, '',false
+//true <~ それ以外
+
+// console.log(Boolean(0)); //false
+// console.log(Boolean('hello')); //true
