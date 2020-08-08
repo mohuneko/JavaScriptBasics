008347163e6769377122f463718653c276ac41f7
mohuneko <b0wdmohu@gmail.com>
2020-08-08 20:20:13 +0900

    データ型

--- js/min.js
+++ js/min.js
@@ -20,4 +20,40 @@
 // console.log(2 + 10 * 3); //32
 // console.log((2 + 10) * 3); //36
 
-//定数
+//定数 const
+// console.log(150 * 140);
+// console.log(150 * 160);
+//変数 let
+
+// let price = 150;
+// console.log(price * 140);
+// console.log(price * 160);
+
+// price = 170;
+// console.log(price * 140);
+// console.log(price * 160);
+
+
+//変数 let
+// let price = 500;
+
+// //price = price + 100;
+// price += 100;
+
+// //price = price * 2;
+// price *= 2;
+
+// //proce = price +1;
+// //price += 1;
+// price++;
+
+// //price -1;
+// price--;
+
+// console.log(price);
+
+console.log(typeof 'hello');
+console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
