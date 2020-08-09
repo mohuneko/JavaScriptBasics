bb545186de3280e034886ca77a43fab59c58a486
mohuneko <b0wdmohu@gmail.com>
2020-08-09 21:04:10 +0900

    アロー関数

--- js/min.js
+++ js/min.js
@@ -174,3 +174,38 @@
 //   }
 //   console.log(i);
 // }
+
+// function showAd(message = 'AD') { // 仮引数
+//   console.log('----------');
+//   console.log(`--- ${message} ---`);
+//   console.log('----------');
+// }
+
+// showAd('Header Ad'); // 実引数
+// console.log('Tom is great!');
+// console.log('Bob is great!');
+// // showAd('Ad');
+// showAd();
+// console.log('Steve is great!');
+// console.log('Richard is great!');
+// showAd('Footer Ad');
+
+
+// function sum(a, b, c) {
+//   return a + b + c;
+// }
+// const sum = function(a, b, c) {
+//   return a + b + c;
+// };
+
+// const sum = (a, b, c) => a + b + c;
+
+
+// const total = sum(1, 2, 3) + sum(3, 4, 5);
+// console.log(total);
+
+// const double = function(a) {
+//   return a * 2;
+// }
+const double = a => a * 2;
+console.log(double(12));
